3d5a11e6954d86ac3d00eaba52674442da3f784c
Tomas Jirsik <jirsik@ics.muni.cz>
2018-05-30 23:41:12 +0200

    Added saving to db

---
+++ db.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+class Database:
+    def __init__(self, database):
+        self.db_conn = sqlite3.connect(database)
+        self.cursor = self.db_conn.cursor()
+
+
+    def createTable(self, table_name, column_list):
+        """
+        Create database table
+        :param table_name: name of the table
+        :param column_list: list of columns
+        :return:
+        """
+        query = "CREATE TABLE IF NOT EXISTS "+ table_name + " (" + column_list + ")"
+        self.cursor.execute(query)
+
+    def insertRecord(self, table_name, data):
+        """
+        Insert record if not exists
+        :param table_name: table name
+        :param data: data in column lists
+        :return:
+        """
+        columns = ', '.join(data.keys())
+        placeholders = ':'+', :'.join(data.keys())
+        # Check if record with a given id not exists
+        query = "SELECT * FROM %s WHERE id LIKE '%s'" % (table_name, data["id"])
+        self.cursor.execute(query)
+        query_result = self.cursor.fetchall()
+        if not query_result:
+            print("Inserting new:", data)
+            query = 'INSERT INTO %s (%s) VALUES (%s)' % (table_name, columns, placeholders)
+            self.cursor.execute(query, data)
+            self.db_conn.commit()
+        else:
+            print("Already exists: ", data)
+
+    def getAllData(self, table_name):
+        """
+
+        :param table_name: name of the table
+        :return:
+        """
+        query = 'SELECT * FROM %s' % (table_name)
+        query_result = self.cursor.execute(query)
+        result = []
+        for record in query_result:
+            result.append(record)
+        return result
+
+    def closeConnection(self):
+        self.db_conn.close()
--- sbazar.py
+++ sbazar.py
@@ -1,10 +1,18 @@
 import urllib.request
 import json
-
+import db
+import hashlib
 
 search_keys = ["touran","xc90"]
 price_from = 80000
 price_to = 250000
+
+advert_record = {"id": "" ,"type": "","create_date": "" ,"price": "","name":"","url_advert": "","url_image":""}
+
+database = db.Database("sbazar-auto.sql")
+column_list = ','.join(advert_record.keys())
+print(column_list)
+database.createTable("advertisement", column_list)
 
 
 for search_phrase in search_keys:
@@ -20,6 +28,19 @@ for search_phrase in search_keys:
             url_image = "https:" + advertisement["images"][0]["url"] + "?fl=exf|crr,1.33333,2|res,800,600,1|wrm,/watermark/sbazar.png,10,10|jpg,80,,1"
         else:
             url_image = "N/A"
+
+        advert_record["id"] = hashlib.sha256(url.encode()).hexdigest()
+        advert_record["type"] = search_phrase
+        advert_record["create_date"] = advertisement["create_date"]
+        advert_record["price"] = advertisement["price"]
+        advert_record["name"] = advertisement["name"]
+        advert_record["url_advert"] = url
+        advert_record["url_image"] = url_image
+        database.insertRecord("advertisement", advert_record)
         print(advertisement["create_date"],advertisement["price"], advertisement["name"], url, url_image)
 
 
+table = database.getAllData("advertisement")
+database.closeConnection()
+
+print(table)
